863b6dbba09e5bee48f1aad067889236a8f72d70
FlejeCute <abigailluciaojedaalonso@alumno.ieselrincon.es>
2024-03-18 20:38:35 +0000

    stars wars wip

---
+++ Assets/Scripts/EasterEggController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggController : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Algooo");
+        if (other.gameObject.CompareTag("EasterText"))
+        {
+            SceneController.instance.LoadScene("GameScene");
+        }
+    }
+}
